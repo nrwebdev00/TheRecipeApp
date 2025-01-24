a0a812f439f85f251121fe5461c7c87b87557f82
Nathon Reed <nrwebdev00@gmail.com>
2025-01-23 19:55:55 -0800

    added pagination to the projects -- /recipe/list -- displays 4 recipes on page anything greater than 4 will show pagination and if count is 4 or less will not show pagination

--- Recipe/views.py
+++ Recipe/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
 from .models import Recipe, Directions,Ingredient,Recipe_Images
 
 def index(request):
@@ -8,8 +9,24 @@ def index(request):
 
 
 def recipe_list(request):
-    recipes = Recipe.objects.all()
-    context = {'recipes': recipes,}
+    recipes_list = Recipe.objects.all()
+    # Pagination
+    paginator = Paginator(recipes_list, 4)
+    object_count = paginator.count
+    per_page = paginator.per_page
+    page_number = request.GET.get('page', 1)
+
+
+
+    # Error Handling on Pagination -
+    try:
+        recipes = paginator.page(page_number)
+    except PageNotAnInteger:
+        recipes = paginator.page(1)
+    except EmptyPage:
+        recipes = paginator.page(paginator.num_pages)
+
+    context = {'recipes': recipes,'object_count':object_count, 'per_page':per_page,}
     return render(request, 'Recipe/list.html', context)
 
 def recipe_detail(request, year,month,day,recipe):
